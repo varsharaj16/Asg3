c8032628135ee2c657ba1ff8d5c7232930bdfa0b
varsharaj16 <vr1692005@gmail.com>
2026-04-01 00:57:35 -0500

    Fix home route message for Flask API
    Updated the home route message to indicate success.

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_albums():
 @app.route('/')
 def home():
     return jsonify({
-        "message": "Flask API is running",
+        "message": "Flask API is successfull",
         "available_routes": ["/posts", "/comments", "/albums"]
     })
 
